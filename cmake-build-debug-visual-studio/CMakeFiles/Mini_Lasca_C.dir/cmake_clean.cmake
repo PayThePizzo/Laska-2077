0af3326d9a0f72f6e432f486d244cd82c1049fba
PayThePizzo <gianmariapizzo@gmail.com>
2020-12-26 13:01:17 +0100

    New Header, Ordered Code, New UI
    Javadoc Style Documentation
    New UI
    New Header
    Code Compiles

---
+++ cmake-build-debug-visual-studio/CMakeFiles/Mini_Lasca_C.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mini_Lasca_C.dir/laska.c.obj"
+  "CMakeFiles/Mini_Lasca_C.dir/main.c.obj"
+  "Mini_Lasca_C.exe"
+  "Mini_Lasca_C.exe.manifest"
+  "Mini_Lasca_C.lib"
+  "Mini_Lasca_C.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Mini_Lasca_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
